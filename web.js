017c79849f1debf3c3910cc28fe8fa57b4c67505
Larry Price <lrprice@sep.com>
2016-01-29 15:40:10 -0500

    previous quarters, years, etc

--- web.js
+++ web.js
@@ -3,6 +3,8 @@ var express = require('express'),
   jade = require('jade'),
   crypto = require('crypto');
 
+require('array.prototype.find');
+
 mongoose.connect('mongodb://localhost/test');
 var db = mongoose.connection;
 
@@ -35,7 +37,7 @@ var getRandomInt = function(bound) {
 
 var app = express();
 app.use(express.static('public'));
-app.get('/', function(req, res) {
+app.get('/forever', function(req, res) {
   Person.findOneRandom(function(err, person) {
     if (err) {
       res.send(err);
@@ -52,6 +54,53 @@ app.get('/', function(req, res) {
       answer: response.answer
     }));
   })
+});
+
+var getQuarter = function (d) {
+  d = d || new Date(); // If no date supplied, use today
+  var q = [1,2,3,4];
+  return {year: d.getFullYear(), quarter: q[Math.floor(d.getMonth() / 3)]};
+};
+
+app.get('/:year(\\d{4})?/:quarter(\\d)?', function(req, res) {
+  console.log("burp")
+  var year = req.params.year, quarter = req.params.quarter, filters = {"year": year, "quarter": quarter};
+  if (!req.params.year) { // use previous quarter
+    var d = new Date();
+    d.setMonth(d.getMonth() - 3);
+    var q = getQuarter(d);
+    year = q.year
+    quarter = q.quarter;
+    filters = {"year": year, "quarter": quarter}
+  } else if (!req.params.quarter) { // use entire year
+    filters = {"year": year}
+  }
+
+  Person.findOneRandom({surveys: {"$elemMatch": filters}}, {}, {}, function(err, person) {
+    if (err) {
+      res.send(err);
+      return;
+    } else if (!person) {
+      res.send("No survey results for the current quarter. Try /forever instead.");
+      return;
+    }
+
+    var survey = person.surveys.find(function(el) {
+      return quarter ? el.year == year && el.quarter == quarter : el.year == year;
+    });
+    var response = survey.responses[getRandomInt(survey.responses.length)];
+
+    res.send(jade.renderFile('views/index.jade', {
+      displayName: person.name,
+      avatarURL: "https://www.gravatar.com/avatar/" + crypto.createHash('md5').update(person.email.trim().toLowerCase()).digest('hex') + "?r=pg&d=retro&s=512",
+      question: response.question,
+      answer: response.answer
+    }));
+  });
+});
+
+app.get('/faq', function(req, res) {
+  res.send(jade.renderFile('views/what.jade'));
 });
 
 app.listen(3000, function() {
